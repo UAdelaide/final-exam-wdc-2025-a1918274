e03613e5556330fa7fcb84411ec32a671a0add7d
a1918274 <a1918274@student.adelaide.edu.au>
2025-06-20 11:26:23 +0000

    [automatic commit] 2025-06-20 11:26:23 UTC 129.127.202.106

--- part2/public/javascripts/logout.js
+++ part2/public/javascripts/logout.js
@@ -1,25 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const logoutBtn = document.getElementById('logoutBtn');
-    if(logoutBtn){
-        logoutBtn.addEventListener('click', async () => {})
+    if (logoutBtn) {
+        logoutBtn.addEventListener('click', async () => {
+            async function logout() {
+                try {
+                    const response = await fetch('/api/users/logout', {
+                        method: 'POST',
+                        credentials: 'include'
+                    });
+
+                    const result = await response.json();
+
+                    if (!response.ok) throw new Error(result.error || 'Logout failed');
+
+                    console.log('Logged out!');
+                    window.location.href = '/';
+
+                } catch (e) {
+                    console.error('error for log out:', e);
+                }
+            });
     }
-    async function logout() {
-    try {
-        const response = await fetch('/api/users/logout', {
-            method: 'POST',
-            credentials: 'include'
-        });
-
-        const result = await response.json();
-
-        if(!response.ok) throw new Error(result.error || 'Logout failed');
-
-console.log('Logged out!');
-window.location.href = '/';
-
-    } catch (e) {
-    console.error('error for log out:', e);
-}
-
 })
