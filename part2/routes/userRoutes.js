5aa365e2f0a534d20621e6f15315be59bf46190b
a1918274 <a1918274@student.adelaide.edu.au>
2025-06-20 11:48:26 +0000

    [automatic commit] 2025-06-20 11:48:26 UTC 129.127.202.106

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -62,6 +62,8 @@ router.post('/login', async (req, res) => {
 });
 
 // POST logout
-router.post('/logout', async (req, res))
+router.post('/logout', async (req, res) => {
+// destroy sessions
+});
 
 module.exports = router;
