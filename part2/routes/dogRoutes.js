c94bedebcaa98cfd5bd7c47532af26a81bd57119
a1918274 <a1918274@student.adelaide.edu.au>
2025-06-20 14:14:20 +0000

    [automatic commit] 2025-06-20 14:14:20 UTC 27.33.127.65

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -6,9 +6,10 @@ const db = require('../models/db');
 router.get('/', async (req, res) => {
   try {
     const [dogs] = await db.query('SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username FROM Dogs JOIN Users ON Dogs.owner_id = Users.user_id');
-    res.json(dogs);
-  } catch (err) {
-    res.status(500).json({ error: 'Failed to fetch dogs' });
+    res.json(rows);
+  } catch (error) {
+    console.error('SQL Error:', error);
+    res.status(500).json({ error: 'Failed to fetch walk requests' });
   }
 });
 
