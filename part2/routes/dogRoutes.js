5c5e516b8482893102d8adf76b4daf39e7b27d5e
a1918274 <a1918274@student.adelaide.edu.au>
2025-06-20 14:06:17 +0000

    [automatic commit] 2025-06-20 14:06:17 UTC 27.33.127.65

---
+++ part2/routes/dogRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models/db');
+
+// Route to return dogs as JSON
+router.get('/api/dogs', async (req, res) => {
+  try {
+    const [dogs] = await db.execute('SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username FROM Dogs JOIN Users ON Dogs.owner_id = Users.user_id');
+    res.json(dogs);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch dogs' });
+  }
+});
