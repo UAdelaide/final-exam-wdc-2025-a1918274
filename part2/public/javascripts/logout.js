23ca04b27cd125a854d3cc0d455f691b50a83e2b
a1918274 <a1918274@student.adelaide.edu.au>
2025-06-20 11:20:00 +0000

    [automatic commit] 2025-06-20 11:20:00 UTC 129.127.202.106

--- part2/public/javascripts/logout.js
+++ part2/public/javascripts/logout.js
@@ -2,7 +2,10 @@ const logoutBtn = document.getElementById('logoutBtn');
 
 async function logout() {
     try {
-        const response = await 
+        const response = await fetch('/api/users/logout', {
+            method: 'POST',
+            headers
+        })
     } catch(e) {
 
     }
