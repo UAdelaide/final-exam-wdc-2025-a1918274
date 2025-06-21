9f11ce704548c0423d2d7ee56a4c3fb12a3c8ab5
a1918274 <a1918274@student.adelaide.edu.au>
2025-06-21 02:56:52 +0000

    [automatic commit] 2025-06-21 02:56:52 UTC 27.33.127.65

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -26,8 +26,8 @@ router.get('/mywalkrequests', async (req, res) => {
       FROM WalkRequests wr
       JOIN Dogs d ON wr.dog_id = d.dog_id
       JOIN Users u ON d.owner_id = u.user_id
-      WHERE wr.status = 'open' AND 
-    `);
+      WHERE wr.status = 'open' AND d.owner_id = ?
+    `, [onwebkittransitionend.i]);
     res.json(rows);
   } catch (error) {
     console.error('SQL Error:', error);
