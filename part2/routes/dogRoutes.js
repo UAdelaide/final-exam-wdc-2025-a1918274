e72fe5642de8acf2bd1226db0f0fb2bbc5c48b6c
a1918274 <a1918274@student.adelaide.edu.au>
2025-06-21 02:03:10 +0000

    [automatic commit] 2025-06-21 02:03:10 UTC 27.33.127.65

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -15,7 +15,10 @@ router.get('/', async (req, res) => {
 
 // route to post owner's dog for owner dashboard (question 15)
 router.post('/mydog', async (req, res) => {
-if(!req.session.user || !req.session.user.role !== '')
+if(!req.session.user || !req.session.user.role !== 'owner'){
+  return res.status(403).json({ error: 'not authorised' });
+}
+
 });
 
 module.exports = router;
