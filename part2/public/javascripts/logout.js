8e08921fa94b7c2adadb10e858aa55f47486ab08
a1918274 <a1918274@student.adelaide.edu.au>
2025-06-21 03:07:17 +0000

    [automatic commit] 2025-06-21 03:07:17 UTC 27.33.127.65

--- part2/public/javascripts/logout.js
+++ part2/public/javascripts/logout.js
@@ -11,7 +11,6 @@ async function logout() {
 
         if (!response.ok) throw new Error(result.error || 'Logout failed');
 
-        alert('Logged out!');
         // logged out, back to home page
         window.location.href = '/';
     } catch (e) {
